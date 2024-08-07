0a5a1b02443b05ccda4825a1330415e54559d6e8
tingtingwuwu <152572752+tingtingwuwu@users.noreply.github.com>
2024-08-07 19:34:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import pandas as pd
-from src.data.data_loader import load_data
-from src.data.data_preprocessing import preprocess_data, fetch_smiles
-from src.models.algorithm import extract_features, evaluate_with_lazy_regressor, feature_selection_and_optimization
+from data_loader import load_data
+from data_preprocessing import preprocess_data, fetch_smiles
+from algorithm import extract_features, evaluate_with_lazy_regressor, feature_selection_and_optimization
 from transformers import AutoTokenizer, RobertaModel, BertModel
 import torch
 import argparse
 
-
 def main():
     parser = argparse.ArgumentParser(description="Choose model for feature extraction")
-    parser.add_argument('--model', type=str, choices=['kbert', 'chemberta'], default='chemberta',
-                        help="Model to use for feature extraction")
+    parser.add_argument('--model', type=str, choices=['kbert', 'chemberta'], default='chemberta', help="Model to use for feature extraction")
     args = parser.parse_args()
-
+    
     input_file_path = 'path/to/compound_data.xlsx'
 
     df = load_data(input_file_path)
     df = preprocess_data(df)
     compound_names = df.iloc[:, :2].values.tolist()
     smiles_list = fetch_smiles(compound_names)
-
+    
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     if args.model == 'kbert':
         tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
@@ -30,10 +28,10 @@ def main():
     elif args.model == 'chemberta':
         tokenizer = AutoTokenizer.from_pretrained("DeepChem/ChemBERTa-77M-MLM")
         model = RobertaModel.from_pretrained("DeepChem/ChemBERTa-77M-MLM")
-
+        
     model.to(device)
     model.eval()
-
+    
     smiles1 = df['SMILES1'].tolist()
     smiles2 = df['SMILES2'].tolist()
     features_smiles1 = extract_features(smiles1, model, tokenizer, device)
@@ -44,7 +42,6 @@ def main():
 
     evaluate_with_lazy_regressor(features, melting_temps)
     feature_selection_and_optimization(features, melting_temps)
-
 
 if __name__ == "__main__":
     main()
